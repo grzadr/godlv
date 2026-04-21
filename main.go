f229ea34dbacaf4d28df8a4478cecf080d9828b8
Adrian Grzemski <adrian.grzemski@gmail.com>
2026-04-21 09:51:57 +0200

    Enhanced App with logging

--- main.go
+++ main.go
@@ -1,9 +1,39 @@
-package godlv
+package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 )
 
+type App struct {
+	logLevel *slog.LevelVar
+	logger   *slog.Logger
+}
+
+func NewApp(level slog.Level) *App {
+	lvl := new(slog.LevelVar)
+	lvl.Set(level)
+
+	return new(App{
+		logLevel: lvl,
+		logger: slog.New(
+			slog.NewTextHandler(
+				os.Stdout,
+				new(slog.HandlerOptions{Level: lvl}),
+			),
+		),
+	})
+}
+
+func (a *App) Debug(msg string, args ...any) {
+	a.logger.Debug(msg, args...)
+}
+
+func (a *App) Info(msg string, args ...any) {
+	a.logger.Info(msg, args...)
+}
+
 func main() {
-	fmt.Println("hello world")
+	app := NewApp(slog.LevelInfo)
+	app.Info("hello world")
 }
