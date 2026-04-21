2a0581779481359bf543620c714eb7dc24638e85
Adrian Grzemski <adrian.grzemski@gmail.com>
2026-04-21 10:00:45 +0200

    Organized files

---
+++ internal/app/app.go
@@ -0,0 +1,38 @@
+package app
+
+import (
+	"log/slog"
+	"os"
+)
+
+type App struct {
+	logLevel *slog.LevelVar
+	logger   *slog.Logger
+}
+
+func NewApp(level slog.Level) *App {
+	lvl := new(slog.LevelVar)
+	lvl.Set(level)
+
+	return new(App{
+		logLevel: lvl,
+		logger: slog.New(
+			slog.NewTextHandler(
+				os.Stdout,
+				new(slog.HandlerOptions{Level: lvl}),
+			),
+		),
+	})
+}
+
+func (a *App) Debug(msg string, args ...any) {
+	a.logger.Debug(msg, args...)
+}
+
+func (a *App) Info(msg string, args ...any) {
+	a.logger.Info(msg, args...)
+}
+
+func (a *App) SetLevel(level slog.Level) {
+	a.logLevel.Set(level)
+}
--- main.go
+++ main.go
@@ -2,38 +2,11 @@ package main
 
 import (
 	"log/slog"
-	"os"
+
+	"github.com/grzadr/godlv/internal/app"
 )
 
-type App struct {
-	logLevel *slog.LevelVar
-	logger   *slog.Logger
-}
-
-func NewApp(level slog.Level) *App {
-	lvl := new(slog.LevelVar)
-	lvl.Set(level)
-
-	return new(App{
-		logLevel: lvl,
-		logger: slog.New(
-			slog.NewTextHandler(
-				os.Stdout,
-				new(slog.HandlerOptions{Level: lvl}),
-			),
-		),
-	})
-}
-
-func (a *App) Debug(msg string, args ...any) {
-	a.logger.Debug(msg, args...)
-}
-
-func (a *App) Info(msg string, args ...any) {
-	a.logger.Info(msg, args...)
-}
-
 func main() {
-	app := NewApp(slog.LevelInfo)
+	app := app.NewApp(slog.LevelInfo)
 	app.Info("hello world")
 }
